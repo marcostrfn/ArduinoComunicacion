f30b7eb9c87df845655cacdfc602cfbf5bb1a424
marcostrfn <marcos.trfn@gmail.com>
2017-04-28 13:47:59 +0200

    Add files via upload

--- ArduinoSketch/RXTXJavaComunicacion/RXTXJavaComunicacion.cpp
+++ ArduinoSketch/RXTXJavaComunicacion/RXTXJavaComunicacion.cpp
@@ -16,32 +16,36 @@ void RXTXJavaComunicacion::init()
 	Serial.begin(_velocidad);   
 }
 
+void RXTXJavaComunicacion::sendByte(byte v)
+{
+	Serial.write(0x01);
+	Serial.write(v);	
+}
+
 void RXTXJavaComunicacion::sendInt(int v)
 {   
    _myInt.i = v;
+   Serial.write(0x02);
    Serial.write(_myInt.b,sizeof(_myInt.b));
 }
 
 void RXTXJavaComunicacion::sendFloat(float v)
 {  
    _myFloat.f = v;
+   Serial.write(0x03);
    Serial.write(_myFloat.b,sizeof(_myFloat.b));
 }
 
 void RXTXJavaComunicacion::sendString(String s)
 {	
-	int l = s.length();
-	char charBuf[l];
-    s.toCharArray(charBuf, l);
+	int l = s.length();	
+	Serial.write(0x05);
+	_myInt.i = l;
+	Serial.write(_myInt.b,sizeof(_myInt.b));
+	Serial.print(s);
 
-	for (int i=0;i<l;i++) {
-		Serial.write(charBuf[i]);
-	}   
 }
 
-void RXTXJavaComunicacion::sendByte(byte v)
-{
-	Serial.write(v);	
-}
+
 
 

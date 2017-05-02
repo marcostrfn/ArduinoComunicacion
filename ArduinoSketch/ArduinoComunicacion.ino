ce940d0c3a55794250d8c6f191e420103c2eeccb
marcostrfn <marcos.trfn@gmail.com>
2017-05-02 21:44:46 +0200

    Add files via upload

--- ArduinoSketch/ArduinoComunicacion.ino
+++ ArduinoSketch/ArduinoComunicacion.ino
@@ -1,61 +1,84 @@
 
 
-// para datos llegando por el puerto serie.
-byte inComingByte;   
-byte outComingByte;   
+#include <RXTXJavaComunicacion.h>
 
-typedef union
-{
-  int i;
-  byte b[2];
-} INTUNION_t;
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x20 for a 16 chars and 2 line display
 
-typedef union
-{
-  float f;
-  byte b[4];
-} FLOATUNION_t;
 
-INTUNION_t myInt;
-FLOATUNION_t myFloat;
+
+RXTXJavaComunicacion rxtx(9600);
+
+byte inComingByte;
+byte outComingByte;
+
+int myInt;
+float myFloat;
+int recibidos;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  myInt.i = 0;
-  myFloat.f = .0;
+
+  rxtx.init();
+
+  myInt = 0;
+  myFloat = .0;
   inComingByte = 0;
-  outComingByte = B00000001; 
+  outComingByte = B00000001;
+
+  lcd.init(); // initialize the lcd
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Marcos Trampal");
+  lcd.setCursor(0, 1);
+  lcd.print("Arduino");
+  lcd.setCursor(0, 2);
+  lcd.print("Test");
+
+
 }
 
 
-void modoBytes() {  
-  myInt.i++;
-  myFloat.f = myFloat.f + 2;  
-  Serial.write(outComingByte);
-  Serial.write(myInt.b,sizeof(myInt.b));
-  Serial.write(myFloat.b,sizeof(myFloat.b));
+void modoBytes() {
+  myInt++;
+  myFloat = myFloat + 2;
+  rxtx.sendByte(outComingByte);
+  rxtx.sendInt(myInt);
+  rxtx.sendFloat(myFloat);
+
+  char buffer[50];
+  sprintf(buffer, "Arduino %i %i", myInt, 6);
+  rxtx.sendString(buffer);
+
 }
 
 void modoString() {
- int sensorValue = analogRead(A0);
- Serial.print("El resultado de la lectura: ");
- Serial.println(sensorValue); 
+  int sensorValue = analogRead(A0);
+  rxtx.sendString("El resultado de la lectura: ");
+  rxtx.sendByte(0x0D);
+  rxtx.sendByte(0x0A);
 }
 
-void modoRead() { 
- outComingByte = inComingByte;
+void modoRead() {
+  outComingByte = inComingByte;
+  //char buffer[20];
+  //sprintf(buffer, "Recibido: %s %4d", outComingByte, recibidos++);  
+  lcd.setCursor(0, 3);
+  lcd.print("Recibido ");
+  lcd.print(outComingByte, DEC);
+  lcd.print(" ");
+  lcd.print(recibidos++);   
 }
 
 
 void loop() {
   // lectura por serie
   while (Serial.available() > 0)
-  { 
-    inComingByte = Serial.read();    
+  {
+    inComingByte = Serial.read();
     modoRead();
   }
-  modoBytes();  
-  // modoString();  
+  modoBytes();
+  // modoString();
   delay(1000);
 }
